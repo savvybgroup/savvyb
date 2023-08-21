cf144af55dbecee1455ef6f79674e092c91c7fea
Samuel <sferreira@savvyb.com.br>
2023-08-21 15:06:49 +0100

    Savvyb V1

---
+++ savvyb.js
@@ -0,0 +1,26 @@
+document.querySelectorAll('input').forEach(($input) => { })
+const field = $input.dataset.js
+$input.addEventListener('input', (e) => {
+
+    e.target.value = formato[field](e.target.value)
+
+}, false)
+const formato = {
+
+    phone(value) {
+
+        return value
+
+            .replace(/\D/g, '')
+
+            .replace(/(\d{2})(\d)/, '($1)$2')
+
+            .replace(/(\d{4})(\d)/, '$1-$2')
+
+            .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+
+            .replace(/(-\d{4})\d+?$/, '$1')
+
+    }
+
+}
