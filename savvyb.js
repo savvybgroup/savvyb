a6d210f29ded0e7c9bb5d8fbe9405e3f5d6baaf0
Samuel <sferreira@savvyb.com.br>
2023-08-24 17:29:16 +0100

    Savvyb_V2

--- savvyb.js
+++ savvyb.js
@@ -24,3 +24,22 @@ const formato = {
     }
 
 }
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const name = document.querySelector('input[name=name]').value;
+    const email = document.querySelector('input[name=email]').value;
+    const phone = document.querySelector('input[name=phone]').value;
+    const request = document.querySelector('input[name=Requests?]').value;
+
+    fetch('https://api.sheetmonkey.io/form/nAtLHoXu1kVbbpPbE7VvcQ', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({ name, email, phone, request }),
+    });
+
+}
+document.querySelector('#insert_form').addEventListener('submit', handleSubmit)
